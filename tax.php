bedbde0b362ed041267d17bb11f60ce3a6d3808d
krisshayden <shupingsnobeck58@gmail.com>
2024-10-27 22:15:20 +0000

    Add files via upload

---
+++ tax.php
@@ -0,0 +1,349 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Accountingproserv.com | Accounting Service In US CANADA</title>
+<!-- for-mobile-apps -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="description" content="Accounting pro provide you accounting services , financial service , business consulting services in USA">
+<meta name="keywords" content="Account Services in USA , Tax Services in Usa , Tex Services in USA , Financial Services , Consulting Services , BPO services , payroll services in USA">
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+		function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- //for-mobile-apps -->
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+<link href="css/font-awesome.min.css" rel="stylesheet" type="text/css" media="all">
+<link rel="stylesheet" href="css/chocolat.css" type="text/css" media="screen">
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<!--fonts-->
+<link href="//fonts.googleapis.com/css?family=Noto+Sans:400,700" rel="stylesheet">
+<link href="//fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet">
+<!--//fonts-->
+
+</head>
+<body>
+<!--Header-->
+	<div class="header" id="home">
+		<!--Top-Bar-->
+				<div class="top-bar">
+				<div class="container-fluid">
+					<div class="header-nav">
+						<nav class="navbar navbar-default">
+							<!-- Brand and toggle get grouped for better mobile display -->
+							<div class="navbar-header">
+								<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+									<span class="sr-only">Toggle navigation</span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+								</button>
+								<h1><a class="navbar-brand" href="index.php">Accounting <span>Services</span></a></h1>
+							</div>
+							<!-- Collect the nav links, forms, and other content for toggling -->
+							<div class="collapse navbar-collapse nav-wil" id="bs-example-navbar-collapse-1">
+								<nav class="cl-effect-15" id="cl-effect-15">
+								<ul>
+									<li><a href="index.php" data-hover="Home">Home</a></li>
+									<li><a href="index.php" data-hover="About">About</a></li>
+									<li><a href="#services" class="scroll" data-hover="Services">Services</a></li>									
+									<li><a href="#contact" class="scroll" data-hover="Contact">Contact</a></li>
+									<li><a href="privacy.php" data-hover="Privacy">Privacy</a></li>
+									<li><a href="terms.php"  data-hover="Terms">Terms</a></li>
+									<li><a href="disclaimer.php" data-hover="Disclaimer">Disclaimer</a></li>
+								</ul>
+								</nav>
+							</div>
+							<div class="agileits-contact-info text-right">
+							<ul>
+								<li><span class="glyphicon glyphicon-earphone" aria-hidden="true"></span>+1-888-546-9721</li>
+							</ul>
+						</div>
+						</nav>
+						<div class="w3ls_search">
+													<div class="cd-main-header">
+														<ul class="cd-header-buttons">
+															<li><a class="cd-search-trigger" href="#cd-search"> <span></span></a></li>
+														</ul> <!-- cd-header-buttons -->
+													</div>
+													<div id="cd-search" class="cd-search">
+														<form action="#" method="post">
+															<input name="Search" type="search" placeholder="Search...">
+														</form>
+													</div>
+												</div>
+					</div>
+					</div>
+			</div>
+		<!--//Top-Bar-->
+		<!--Slider-->
+		<div class="slider">
+			<div class="callbacks_container">
+				<ul class="rslides" id="slider">
+					<li>
+						<div class="slider-img">
+							<img src="images/bann2.jpg" class="img-responsive" alt="Trade Zone">
+						</div>
+						<div class="slider-info">
+							<img src="images/logo1.png" class="img-responsive" alt="logo">
+							<h3>A Global Accounting Service Provider</h3>
+						    <p>Affordable Accounting for your Business</p>
+							<h3><span class="glyphicon glyphicon-earphone"></span> +1-888-546-9721 </h3>
+						</div>
+					</li>
+					<li>
+						<div class="slider-img">
+							<img src="images/bann1.jpg" class="img-responsive" alt="Trade Zone">
+						</div>
+						<div class="slider-info">
+							<img src="images/logo1.png" class="img-responsive" alt="logo">
+							<h3>Financial Services</h3>
+						    <p>Accounting Services for CPAs, Firms and Individuals</p>
+							<h3><span class="glyphicon glyphicon-earphone"></span> +1-888-546-9721 </h3>
+						</div>
+					</li>
+					<li>
+						<div class="slider-img">
+							<img src="images/bann2.jpg" class="img-responsive" alt="Trade Zone">
+						</div>
+						<div class="slider-info">
+							<img src="images/logo1.png" class="img-responsive" alt="logo">
+							<h3>Business Process Services</h3>
+						    <p>Transaction Processing , Data Digitization , Transcription , Internet Research</p>
+							<h3><span class="glyphicon glyphicon-earphone"></span> +1-888-546-9721 </h3>
+						</div>
+					</li>
+					<li>
+						<div class="slider-img">
+							<img src="images/bann1.jpg" class="img-responsive" alt="Trade Zone">
+						</div>
+						<div class="slider-info">
+							<img src="images/logo1.png" class="img-responsive" alt="logo">
+							<h3>Global Accounting Service Provider</h3>
+						    <p>Affordable Accounting for your Business</p>
+							<h3><span class="glyphicon glyphicon-earphone"></span> +1-888-546-9721 </h3>
+						</div>
+					</li>
+
+				</ul>
+				
+			</div>
+			<div class="clearfix"></div>
+		</div>		
+		<!--//Slider-->
+	</div>
+<!--//Header-->
+<!--sevices-->
+<div class="sevices-w3layouts" id="services">
+	<div class="container">
+		<div class="sevices-header-agile">
+			<h3 class="tittle">Main Services</h3>			
+		</div>
+			<div class="row">
+				<div class="col-md-3 sevices-grid ">
+					<div class="icon">
+						<a href="services.php"><i class="fa fa-gavel" aria-hidden="true"></i></a>
+					<h4><a href="services.php">Accounting Services</a></h4>
+					</div>					
+					
+				</div>
+			<div class="col-md-3 sevices-grid ">
+					<div class="icon">						
+					<a href="payroll.php"><i class="fa fa-money" aria-hidden="true"></i></a>
+					<h4><a href="payroll.php">Payroll Services</a></h4>
+					</div>					
+						</div>
+				<div class="col-md-3 sevices-grid ">
+					<div class="icon">						
+						<a href="business.php"><i class="fa fa-users" aria-hidden="true"></i></a>
+						<h4><a href="business.php">Business Consultation</a></h4>
+					</div>					
+						</div>
+				<div class="col-md-3 sevices-grid ">
+					<div class="icon">						
+						<a href="tax.php"><i class="fa fa-clock-o" aria-hidden="true"></i></a>
+						<h4><a href="tax.php">Tax Planing & Preparation</a></h4>
+					</div>				
+					</div>
+				<div class="clearfix"></div>
+				<a href="services.php" class="servicebutton">More Services</a>
+			</div>			
+	</div>
+</div>
+<!-- about -->
+	<div class="about" id="about">
+		<div class="container">
+					<h3 class="tittle">Tax Planning and Preparation</h3>
+						<p class="title-para-w3-agileits">Welcome To Accountingproserv.com</p>
+						<div class="w3l-grids-about">								
+								<div class="col-md-12 w3ls-agile-left">									
+									<p><b>Accounting Professional Service provides comprehensive tax planning and preparation of all tax returns</b></p><br>
+									<h3>Complete Tax Planing and Preparation</h3><br>
+									<p>We are a full-service tax office. If anytime during the year, you have a tax question or concern, call us. We are here to assist you. It is part of our service to provide you tax peace of mind. In fact, we suggest meeting in June or July for a mid-year tax review and planning session. This is one of the best ways you can prepare for next tax season.</p><br>									
+								</div>
+								<div class="clearfix"> </div>								
+			 				</div>
+			   </div>
+		</div>
+<!-- //about-bottom -->
+<!--contact-->
+<div class="contact-w3-agileits" id="contact">
+<div class="container">
+<h3 class="tittle">Our Contacts</h3>
+	<div class="map-left-w3-agile">
+		<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d50100.48997179645!2d-76.53127244240966!3d38.29616554483137!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89b7622e038ee603%3A0x71219558c7232c0!2sCalifornia%2C+MD%2C+USA!5e0!3m2!1sen!2sin!4v1478498810487"></iframe>
+	</div>
+	<div class="address-right-w3ls">
+		<h5>Contacts</h5>
+		<label class="address-bar-w3"></label><h6>Address</h6>
+		<p>39487 gallaudet dr #133 fremont CA 94587</p>
+		<label class="address-bar-w3"></label><h6>Phone numbers</h6>
+		<p>+1-888-546-9721</p>		
+		<label class="address-bar-w3"></label><h6>Email</h6>
+		<p><a href="mailto:info@accountingproserv.com">info@accountingproserv.com</a></p>
+		<label class="address-bar-w3"></label><h6>Working hours</h6>
+		<p>Monday-Friday : 8am-6pm ;</p>
+		<p>Weekend : 10am-4pm ;</p>
+	</div>
+	<div class="clearfix"></div>
+	<div class="form">
+		<section class="content bgcolor-5">
+			<form action="#" method="post">
+				<span class="input input--minoru">
+					<input class="input__field input__field--yoko" type="text" id="input-16" required="" />
+					<label class="input__label input__label--yoko" for="input-16">
+						<span class="input__label-content input__label-content--yoko">Name</span>
+					</label>
+				</span>
+				<span class="input input--minoru">
+					<input class="input__field input__field--yoko" type="email" id="input-17" required="" />
+					<label class="input__label input__label--yoko" for="input-17">
+						<span class="input__label-content input__label-content--yoko">Email</span>
+					</label>
+				</span>
+				<span class="input input--minoru">
+					<input class="input__field input__field--yoko" type="text" id="input-18" required="" />
+					<label class="input__label input__label--yoko" for="input-18">
+						<span class="input__label-content input__label-content--yoko">Phone</span>
+					</label>
+				</span>
+				<textarea name="Message" placeholder="Message..." required="" ></textarea>
+						<input type="submit" value="Submit">
+			</form>
+		</section>
+	</div>
+</div>
+</div>
+
+<!--/contact -->
+<!--footer-->
+<div class="footer">
+		<div class="footer-content-agile">
+			<div class="col-md-3 about-us-agile">
+				<h4>about us</h4>
+					<div class="aboutus-right-wthree">
+						<p>Thank you for visit to Accountingproserv.com We are Providing Accounting Services , Payroll Services , Business Consultant Services , Tax Planing & Preparation Services In United States. Listening to and working with our Customers</p>
+						   
+					</div>
+			</div>
+			<div class="col-md-3 news-letter-agileits-w3layouts">
+				<h4>newsletter</h4>
+				<p>signup for our weekly newsletter and get the latest updates</p>
+					<form action="#" method="post">
+						<input type="email" name="Email" placeholder="Enter your Email" required="" />
+						<input type="submit" value="Subscribe" />
+					</form>
+			</div>
+			<div class="col-md-3 links-w3layouts">
+				<h4>Our links</h4>
+				<ul>
+					<li><a class="scroll active" href="#home"><span>Home</span></a></li> 
+					<li><a class="scroll" href="#services"><span>Services</span></a></li> 										
+					<li><a class="scroll" href="#contact"><span>Contact</span></a></li> 
+										 
+				</ul>
+			</div>
+			<div class="col-md-3 footer-social-icons-agileinfo">
+			<h4>Social media</h4>
+				<ul>
+					<li><a href="#"><span><i class="fa fa-facebook" aria-hidden="true"></i></span>Facebook</a></li>
+					<li><a href="#"><span><i class="fa fa-google-plus" aria-hidden="true"></i></span> Google Plus</a></li>
+					<li><a href="#"><span><i class="fa fa-twitter" aria-hidden="true"></i></span> Twitter</a></li>
+					<li><a href="#"><span><i class="fa fa-pinterest-p" aria-hidden="true"> </i></span>Pinterest</a></li>
+				</ul>
+			</div>
+			<div class="clearfix"> </div>
+			<div class="copy">
+		        <p>© 2017 Accountingproserv.com . All Rights Reserved </p>
+		    </div>
+		</div>
+</div>
+<!--/footer -->
+
+<!-- js -->
+<script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
+
+
+<!-- start-smoth-scrolling -->
+<script type="text/javascript" src="js/move-top.js"></script>
+<script type="text/javascript" src="js/easing.js"></script>
+<script type="text/javascript">
+	jQuery(document).ready(function($) {
+		$(".scroll").click(function(event){		
+			event.preventDefault();
+			$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+		});
+	});
+</script>
+<!-- start-smoth-scrolling -->
+<!--light-box-files -->
+<script src="js/jquery.chocolat.js"></script>
+<!--//light-box-files -->
+		<script type="text/javascript">
+		$(function() {
+			$('.gallery a').Chocolat();
+		});
+		</script>
+<!-- //js -->
+<script src="js/responsiveslides.min.js"></script>
+		<script>
+				$(function () {
+					$("#slider").responsiveSlides({
+						auto: true,
+						pager: true,
+						nav: true,
+						speed: 1000,
+						namespace: "callbacks",
+						before: function () {
+							$('.events').append("<li>before event fired.</li>");
+						},
+						after: function () {
+							$('.events').append("<li>after event fired.</li>");
+						}
+					});
+				});
+			</script>
+		
+		<!--search-bar-->
+		<script src="js/main.js"></script>	
+<!--//search-bar-->
+<!-- smooth scrolling -->
+	<script type="text/javascript">
+		$(document).ready(function() {
+		/*
+			var defaults = {
+			containerID: 'toTop', // fading element id
+			containerHoverID: 'toTopHover', // fading element hover id
+			scrollSpeed: 1200,
+			easingType: 'linear' 
+			};
+		*/								
+		$().UItoTop({ easingType: 'easeOutQuart' });
+		});
+	</script>
+	
+	
+	<a href="#home" id="toTop" style="display: block;"> <span id="toTopHover" style="opacity: 1;"> </span></a>
+<!-- //smooth scrolling -->
+<script type="text/javascript" src="js/bootstrap-3.1.1.min.js"></script>
+
+</body>
+</html>
